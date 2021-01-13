9f20152fdae48325345454411d6ad755c584cdf9
Denilson <denilsontivane@gmail.com>
2021-01-13 10:15:47 -0800

    acertos

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://AdminDev:<EOCpaP2SVamqyJUR>@cluster0.sxbie.mongodb.net/<dbname>?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://AdminDev:<EOCpaP2SVamqyJUR>@cluster0.sxbie.mongodb.net/<blogdev>?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/myblog"}
 }
