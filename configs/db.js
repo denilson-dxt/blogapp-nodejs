2bc3aa0107a176d6d6b30e4f53ce70c849f61752
Denilson <denilsontivane@gmail.com>
2021-01-13 10:10:27 -0800

    password acert

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://AdminDev:<25122000xt-blogdev>@cluster0.sxbie.mongodb.net/<dbname>?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://AdminDev:<EOCpaP2SVamqyJUR>@cluster0.sxbie.mongodb.net/<dbname>?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/myblog"}
 }
